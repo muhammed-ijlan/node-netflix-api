0f711aa2f7894a9fd92888e61b1814dc402cc78b
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-28 12:34:16 +0530

    code formatted

--- routes/lists.js
+++ routes/lists.js
@@ -1,39 +1,37 @@
 const router = require("express").Router();
 const { aggregate } = require("../models/List");
-const List = require('../models/List')
+const List = require("../models/List");
 const verify = require("../verifyToken");
 
 //Create Movie
 router.post("/", verify, async (req, res) => {
     if (req.user.isAdmin) {
-        const newList = new List(req.body)
+        const newList = new List(req.body);
 
         try {
             const savedList = await newList.save();
-            res.status(201).json(savedList)
-
+            res.status(201).json(savedList);
         } catch (err) {
-            res.status(500).json(err)
+            res.status(500).json(err);
         }
-
     } else {
         res.status(403).json("You are not allowed to add movie");
     }
 });
 
-//Delete 
+//Delete
 router.delete("/:id", verify, async (req, res) => {
     if (req.user.isAdmin) {
         try {
-            await List.findByIdAndDelete(req.params.id)
-            res.status(201).json("Successfully deleted")
+            await List.findByIdAndDelete(req.params.id);
+            res.status(201).json("Successfully deleted");
         } catch (err) {
-            res.status(500)
+            res.status(500);
         }
     } else {
-        res.status(403).json("You are not able to perform this action")
+        res.status(403).json("You are not able to perform this action");
     }
-})
+});
 
 //Get
 router.get("/", verify, async (req, res) => {
@@ -48,29 +46,24 @@ router.get("/", verify, async (req, res) => {
                 if (genreQuery) {
                     list = await List.aggregate([
                         { $sample: { size: 10 } },
-                        { $match: { type: typeQuery, genre: genreQuery } }
-                    ])
+                        { $match: { type: typeQuery, genre: genreQuery } },
+                    ]);
                 }
 
                 list = await List.aggregate([
                     { $sample: { size: 10 } },
-                    { $match: { type: typeQuery } }
-                ])
-
+                    { $match: { type: typeQuery } },
+                ]);
             } else {
-                list = await List.aggregate([{ $sample: { size: 10 } }])
+                list = await List.aggregate([{ $sample: { size: 10 } }]);
             }
 
-            res.status(200).json(list)
-
+            res.status(200).json(list);
         } catch (err) {
-            res.status(500).json(err)
+            res.status(500).json(err);
         }
-
-    } catch (err) {
-
-    }
-})
+    } catch (err) { }
+});
 //Get Random
 
 module.exports = router;
