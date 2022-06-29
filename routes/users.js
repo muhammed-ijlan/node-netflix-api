f628cf2c35e6a01d442fdf2295ca94d7b8a60583
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-29 13:23:07 +0530

    removed unwanted data from users route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJs = require("crypto-js");
+const CryptoJS = require("crypto-js");
 const verify = require("../verifyToken");
+//UPDATE
 
-//UPDATE USER
 router.put("/:id", verify, async (req, res) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
         if (req.body.password) {
-            req.body.password = CryptoJs.AES.encrypt(
+            req.body.password = CryptoJS.AES.encrypt(
                 req.body.password,
                 process.env.SECRET_KEY
             ).toString();
@@ -16,16 +16,17 @@ router.put("/:id", verify, async (req, res) => {
         try {
             const updatedUser = await User.findByIdAndUpdate(
                 req.params.id,
-                { $set: req.body },
+                {
+                    $set: req.body,
+                },
                 { new: true }
             );
-
-            res.status(201).json(updatedUser);
+            res.status(200).json(updatedUser);
         } catch (err) {
             res.status(500).json(err);
         }
     } else {
-        res.status(403).json("You can update only your account");
+        res.status(403).json("You can update only your account!");
     }
 });
 
@@ -34,23 +35,22 @@ router.delete("/:id", verify, async (req, res) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
         try {
             await User.findByIdAndDelete(req.params.id);
-
-            res.status(201).json("User has been deleted");
+            res.status(200).json("User has been deleted...");
         } catch (err) {
             res.status(500).json(err);
         }
     } else {
-        res.status(403).json("You can delete only your account");
+        res.status(403).json("You can delete only your account!");
     }
 });
 
 //GET
+
 router.get("/find/:id", async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
         const { password, ...info } = user._doc;
-
-        res.status(201).json(info);
+        res.status(200).json(info);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -59,41 +59,24 @@ router.get("/find/:id", async (req, res) => {
 //GET ALL
 router.get("/", verify, async (req, res) => {
     const query = req.query.new;
-
     if (req.user.isAdmin) {
         try {
             const users = query
-                ? await User.find().sort({ _id: -1 }).limit(10)
+                ? await User.find().sort({ _id: -1 }).limit(5)
                 : await User.find();
-
-            res.status(201).json(users);
+            res.status(200).json(users);
         } catch (err) {
             res.status(500).json(err);
         }
     } else {
-        res.status(403).json("You are not allowed to see all users");
+        res.status(403).json("You are not allowed to see all users!");
     }
 });
 
 //GET USER STATS
 router.get("/stats", async (req, res) => {
     const today = new Date();
-    const lastYear = today.setFullYear(today.setFullYear() - 1);
-
-    const monthsArray = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
+    const latYear = today.setFullYear(today.setFullYear() - 1);
 
     try {
         const data = await User.aggregate([
@@ -109,7 +92,7 @@ router.get("/stats", async (req, res) => {
                 },
             },
         ]);
-        res.status(200).json(data);
+        res.status(200).json(data)
     } catch (err) {
         res.status(500).json(err);
     }
