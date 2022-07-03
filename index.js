02603bbe9f935d8e14ce8d464aa541ac6c7ac035
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-03 10:38:00 +0530

    cors middleware added

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+
 //DB Connection
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
