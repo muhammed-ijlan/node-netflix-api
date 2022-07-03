60610c12438d44a9ba19ff3e0166f2d24b183a95
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-03 11:34:39 +0530

    cors middeleware added

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express()
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,**Authorization**')
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
