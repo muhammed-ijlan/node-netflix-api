79b26a1817c3489c24c07a4acdba517656544ee6
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-03 11:16:23 +0530

    cors added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express()
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:4000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
