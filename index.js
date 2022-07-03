df421354795f11822d38be2e928866fe92c6336f
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-03 10:57:54 +0530

    cors removed

--- index.js
+++ index.js
@@ -5,26 +5,10 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const movieRoute = require("./routes/movies")
 const listRoute = require("./routes/lists")
-const cors = require('cors')
 
 dotenv.config();
 
 const app = express()
-
-const corsOptions = {
-    origin: 'https://netflix-mern-2022.herokuapp.com/api/',
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
 
 //DB Connection
 mongoose.connect(process.env.MONGO_URL, {
