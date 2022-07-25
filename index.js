564254d76ed6bd416e938546e42e056d7ca413cf
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-25 20:00:39 +0530

    branch restored

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ dotenv.config();
 const app = express()
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,**Authorization**')
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,**Authorization**')
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
 });
 
 //DB Connection
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => {
-    console.log("DB Connected")
+  console.log("DB Connected")
 }).catch(err => console.log(err))
 
 app.use(express.json())
@@ -35,5 +35,5 @@ app.use("/api/lists", listRoute)
 
 
 app.listen(process.env.PORT || 8800, () => {
-    console.log("Backend server is running");
+  console.log("Backend Server is running");
 })
